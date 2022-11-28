d239f0b8c9493420a7696ef2bb56fa74c73aee5b
fudosman <fudosman@gmail.com>
2022-11-28 08:29:12 +0100

    commented out a secrete

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const version1 = require('./v1/index');
-const version2 = require('./v2/index');
+const version1 = require('./v1');
+const version2 = require('./v2');
 
 const app = express();
 
